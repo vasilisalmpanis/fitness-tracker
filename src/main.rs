e1a02d37fff5ea2aeaf7d0c575ec36409ef01070
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-05-06 18:02:34 +0200

    fitness-tracker: actix-web and website
    change to the actix-web runtime which is much more
    well known in the community and serve a basic static
    html file. This file has all the necessary information for
    the tracker but the backend funtionality is missing.
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,50 @@
-#[macro_use] extern crate nickel;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use rusqlite::{Connection, Result as SqliteResult};
+use chrono::naive::NaiveDate;
+use actix_files::Files;
 
-use nickel::Nickel;
-
-fn say_hello() -> &'static str {
-    "Hello world"
+#[derive(Debug)]
+struct Workout {
+    id: i32,
+    date: NaiveDate,
+    duration: i32 // In minutes
 }
 
-fn main() {
-    let mut server = Nickel::new();
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
 
-    server.utilize(
-        router! (
-            get "**" => |_req, _res| {
-                say_hello()
-            }
-        )
-    );
-    _ = server.listen("127.0.0.1:6767");
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let mut _conn = Connection::open("workout.db").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+    
+    _conn.execute(
+        "CREATE TABLE IF NOT EXISTS workouts (
+            id INTEGER PRIMARY KEY,
+            date DATETIME NOT NULL,
+            duration INTEGER NOT NULL
+        )",
+        [],
+    ).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/", "./static").index_file("index.html"))
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
